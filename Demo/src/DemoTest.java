aa5ccc14731673f06fe1327967e8e49b75f62c08
Admin <Admin@192.168.1.4>
2022-02-05 09:05:45 +0530

    First Commit

---
+++ Demo/src/DemoTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Admin
+ *
+ */
+public class DemoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Git demo");
+	}
+
+}
